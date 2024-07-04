c1ba71e33db09a0e3065f9ad85aa1c59ee327ac9
gabriel_fjorge <gabriel.ferreira.jorge@escola.pr.gov.br>
2024-07-04 20:02:46 -0300

    Colocando visualização de erros e acertos

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const perguntas = [
   {
-    pergunta: "Qual é o papel dos materiais concretos no desenvolvimento do raciocínio lógico-matemático das crianças?",
+    pergunta: "1. Qual é o papel dos materiais concretos no desenvolvimento do raciocínio lógico-matemático das crianças?",
     alternativas: [
       "Estimulam a memorização de fórmulas matemáticas.",
       "Promovem a interação física e sensorial com os conceitos matemáticos.",
@@ -10,7 +10,7 @@ const perguntas = [
     respostaCorreta: "Promovem a interação física e sensorial com os conceitos matemáticos."
   },
   {
-    pergunta: "Como os materiais concretos ajudam na construção de esquemas conceituais nas crianças?",
+    pergunta: "2. Como os materiais concretos ajudam na construção de esquemas conceituais nas crianças?",
     alternativas: [
       "Facilitam a memorização de definições teóricas.",
       "Permitem que as crianças criem representações mentais abstratas.",
@@ -20,7 +20,7 @@ const perguntas = [
     respostaCorreta: "Proporcionam experiências tangíveis que apoiam a compreensão dos conceitos."
   },
   {
-    pergunta: "Por que é importante utilizar materiais concretos para contextualizar o conteúdo de Matemática na educação infantil?",
+    pergunta: "3. Por que é importante utilizar materiais concretos para contextualizar o conteúdo de Matemática na educação infantil?",
     alternativas: [
       "Porque simplificam a compreensão dos conceitos matemáticos.",
       "Porque são mais fáceis de armazenar e organizar.",
@@ -30,7 +30,7 @@ const perguntas = [
     respostaCorreta: "Porque simplificam a compreensão dos conceitos matemáticos."
   },
   {
-    pergunta: "Quais são os benefícios de utilizar materiais concretos para facilitar a compreensão dos conceitos matemáticos?",
+    pergunta: "4. Quais são os benefícios de utilizar materiais concretos para facilitar a compreensão dos conceitos matemáticos?",
     alternativas: [
       "Desenvolvem apenas habilidades motoras das crianças.",
       "Permitem que as crianças visualizem e manipulem os conceitos matemáticos.",
@@ -40,7 +40,7 @@ const perguntas = [
     respostaCorreta: "Permitem que as crianças visualizem e manipulem os conceitos matemáticos."
   },
   {
-    pergunta: "Como os materiais concretos despertam o interesse dos alunos durante as aulas?",
+    pergunta: "5. Como os materiais concretos despertam o interesse dos alunos durante as aulas?",
     alternativas: [
       "Aumentando a dificuldade das atividades matemáticas.",
       "Proporcionando experiências sensoriais e interativas.",
@@ -50,7 +50,7 @@ const perguntas = [
     respostaCorreta: "Proporcionando experiências sensoriais e interativas."
   },
   {
-    pergunta: "De que maneira os materiais concretos tornam as aulas mais dinâmicas e atraentes para as crianças?",
+    pergunta: "6. De que maneira os materiais concretos tornam as aulas mais dinâmicas e atraentes para as crianças?",
     alternativas: [
       "Introduzindo apenas atividades repetitivas.",
       "Permitindo a exploração ativa dos conceitos matemáticos.",
@@ -60,7 +60,7 @@ const perguntas = [
     respostaCorreta: "Permitindo a exploração ativa dos conceitos matemáticos."
   },
   {
-    pergunta: "Quais critérios devem ser considerados ao escolher materiais concretos adequados para atividades educacionais?",
+    pergunta: "7. Quais critérios devem ser considerados ao escolher materiais concretos adequados para atividades educacionais?",
     alternativas: [
       "Facilidade de armazenamento e custo baixo.",
       "Segurança, durabilidade e relevância para o conteúdo ensinado.",
@@ -70,7 +70,7 @@ const perguntas = [
     respostaCorreta: "Segurança, durabilidade e relevância para o conteúdo ensinado."
   },
   {
-    pergunta: "Como os educadores podem integrar materiais concretos às atividades do dia a dia na educação infantil?",
+    pergunta: "8. Como os educadores podem integrar materiais concretos às atividades do dia a dia na educação infantil?",
     alternativas: [
       "Limitando o uso dos materiais concretos para atividades especiais.",
       "Incorporando-os de forma planejada e estruturada nas lições diárias.",
@@ -80,7 +80,7 @@ const perguntas = [
     respostaCorreta: "Incorporando-os de forma planejada e estruturada nas lições diárias."
   },
   {
-    pergunta: "Quais são alguns exemplos práticos de atividades que podem ser realizadas com materiais concretos para ensinar conceitos matemáticos básicos?",
+    pergunta: "9. Quais são alguns exemplos práticos de atividades que podem ser realizadas com materiais concretos para ensinar conceitos matemáticos básicos?",
     alternativas: [
       "Memorização de fórmulas matemáticas.",
       "Representação gráfica de conceitos abstratos.",
@@ -90,7 +90,7 @@ const perguntas = [
     respostaCorreta: "Manipulação de objetos para contar e classificar."
   },
   {
-    pergunta: "Como a interação física com materiais concretos pode promover o aprendizado mais significativo em comparação com métodos puramente teóricos?",
+    pergunta: "10. Como a interação física com materiais concretos pode promover o aprendizado mais significativo em comparação com métodos puramente teóricos?",
     alternativas: [
       "Limitando o desenvolvimento de habilidades práticas.",
       "Apoiando a compreensão dos conceitos através da experiência prática.",
@@ -104,6 +104,7 @@ const perguntas = [
 let indicePerguntaAtual = 0;
 let pontuacao = 0;
 let nome = '';
+let respostas = [];
 
 function iniciarQuiz() {
   const nomeInput = document.getElementById('nome');
@@ -181,7 +182,18 @@ function verificarResposta(inputSelecionado) {
   const respostaCorreta = perguntas[indicePerguntaAtual].respostaCorreta;
 
   if (respostaSelecionada === respostaCorreta) {
-      pontuacao++;
+    pontuacao++;
+    respostas.push({
+      pergunta: perguntas[indicePerguntaAtual].pergunta,
+      resposta: respostaSelecionada,
+      correta: true
+    });
+  } else {
+    respostas.push({
+      pergunta: perguntas[indicePerguntaAtual].pergunta,
+      resposta: respostaSelecionada,
+      correta: false
+    });
   }
   habilitarBotaoAvancar();
 }
@@ -216,7 +228,6 @@ function habilitarBotaoAvancar(){
   botaoAvancar.classList.add('enabled');
 }
 
-
 function finalizarQuiz() {
   document.getElementById('quiz').style.display = 'none';
   const feedbackDiv = document.getElementById('resultado');
@@ -232,6 +243,15 @@ function finalizarQuiz() {
           origin: { y: 0.6 }
       });
   }
+
+  const divRespostas = document.getElementById('respostas');
+  divRespostas.innerHTML = '';
+  
+  respostas.forEach(res => {
+    const divResposta = document.createElement('div');
+    divResposta.textContent = `${res.pergunta} - Sua resposta: ${res.resposta} - ${res.correta ? 'Correta' : 'Incorreta'}`;
+    divRespostas.appendChild(divResposta);
+  });
   
   feedbackDiv.style.display = 'block';
 }
@@ -239,6 +259,7 @@ function finalizarQuiz() {
 document.getElementById('tentarNovamente').addEventListener('click', () => {
   indicePerguntaAtual = 0;
   pontuacao = 0;
+  respostas = [];
   document.getElementById('resultado').style.display = 'none';
   document.getElementById('quiz').style.display = 'block';
   exibirPergunta(indicePerguntaAtual);
@@ -248,6 +269,7 @@ document.getElementById('tentarNovamente').addEventListener('click', () => {
 document.getElementById('reiniciar').addEventListener('click', () => {
   indicePerguntaAtual = 0;
   pontuacao = 0;
+  respostas = [];
   document.getElementById('resultado').style.display = 'none';
   document.getElementById('inicio').style.display = 'block';
   desabilitarBotaoAvancar();
